90d317d688a06df9600018df04f04a90ac19d19f
Benjamin Alan Weaver <baweaver@lbl.gov>
2018-05-23 10:36:21 -0700

    working version

--- app/distributor.py
+++ app/distributor.py
@@ -24,7 +24,11 @@ app = Flask(__name__)
 cache = None
 healpixels = None
 
+
+@app.before_first_request
 def load_app_data():
+    """Get the list of spectra-64 files and initialize the cache.
+    """
     global cache, healpixels
     cache = SimpleCache()
     desi_spectro_redux = os.environ['DESI_SPECTRO_REDUX']
@@ -43,7 +47,9 @@ def load_app_data():
 def index():
     """Return all healpixels.
     """
-    return jsonify(sorted(list(healpixels.keys())))
+    response = jsonify(sorted(list(healpixels.keys())))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 @app.route('/<int:pixel>')
@@ -53,7 +59,8 @@ def spectra(pixel):
     response = cache.get(str(pixel))
     if response is None:
         with fits.open(healpixels[pixel]) as hdulist:
-            response = jsonify(hdulist['B_WAVELENGTH'].header['NAXIS1'])
+            response = jsonify(hdulist['B_FLUX'].header['NAXIS2'])
+            response.headers.add('Access-Control-Allow-Origin', '*')
         cache.set(str(pixel), response, timeout=3600)
     return response
 
@@ -73,9 +80,15 @@ def flux(pixel, spectrum):
                 data[spectrograph]['wavelength'] = hdulist['{0}_WAVELENGTH'.format(spectrograph.upper())].data.tolist()
                 data[spectrograph]['flux'] = hdulist['{0}_FLUX'.format(spectrograph.upper())].data[data['spectrum']].tolist()
                 data[spectrograph]['ivar'] = hdulist['{0}_IVAR'.format(spectrograph.upper())].data[data['spectrum']].tolist()
+            data['desi_target'] = str(hdulist['FIBERMAP'].data[data['spectrum']]['DESI_TARGET'])
+            data['bgs_target'] = str(hdulist['FIBERMAP'].data[data['spectrum']]['BGS_TARGET'])
+            data['mws_target'] = str(hdulist['FIBERMAP'].data[data['spectrum']]['MWS_TARGET'])
+            data['night'] = int(hdulist['FIBERMAP'].data[data['spectrum']]['NIGHT'])
+            data['expid'] = int(hdulist['FIBERMAP'].data[data['spectrum']]['EXPID'])
+            data['tileid'] = int(hdulist['FIBERMAP'].data[data['spectrum']]['TILEID'])
         with fits.open(os.path.join(os.path.dirname(healpixels[pixel]),
                                     os.path.basename(healpixels[pixel]).replace('spectra', 'zbest'))) as hdulist:
-            fibermap = hdulist['FIBERMAP'].data[spectrum['id']]
+            fibermap = hdulist['FIBERMAP'].data[data['spectrum']]
             targetid = int(fibermap['TARGETID'])
             data['targetid'] = str(targetid)
             data['ra'] = float(fibermap['RA_TARGET'])
@@ -89,13 +102,16 @@ def flux(pixel, spectrum):
                 data['zwarn'] = float(row['ZWARN'])
                 data['type'] = str(row['SPECTYPE'][0])
                 data['subtype'] = str(zcatalog[w.nonzero()[0]]['SUBTYPE'][0])
+                # data['desi_target'] = str(row['DESI_TARGET'])
+                # data['bgs_target'] = str(row['BGS_TARGET'])
+                # data['mws_target'] = str(row['MWS_TARGET'])
             # else:
             #     print('Target {0:d} not found in ZCATALOG.'.format(spectrum['targetid']))
         response = jsonify(data)
+        response.headers.add('Access-Control-Allow-Origin', '*')
         cache.set(cacheID, response, timeout=3600)
     return response
 
 
 if __name__ == '__main__':
-    load_app_data()
     app.run(debug=True, host='0.0.0.0', port=56789)
